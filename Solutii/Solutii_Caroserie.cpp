0aecdfb7be93539b622dcd5fbedc4898e4c8bea7
oanagangal <48553814+oanagangal@users.noreply.github.com>
2019-05-21 03:32:21 +0300

    Update Solutii_Caroserie.cpp

--- Solutii/Solutii_Caroserie.cpp
+++ Solutii/Solutii_Caroserie.cpp
@@ -5,7 +5,7 @@
        int Solutii_Caroserie::pret_grund = 60;
        int Solutii_Caroserie::pret_vopsea = 50;
        int Solutii_Caroserie::pret_lac = 20;
-       int Solutii_Caroserie::capota_noua = 480;
+       int Solutii_Caroserie::capota_noua = 420;
        int Solutii_Caroserie::aripa_noua = 300;
        int Solutii_Caroserie::fata_spate = 300;
        int Solutii_Caroserie::manopera = 50;
@@ -21,23 +21,23 @@ double Solutii_Caroserie::indepartare_rugina(int k)
 double Solutii_Caroserie::remediere_lovitura(int k)
 {
     float pas = Solutii_Caroserie::pret_grund + Solutii_Caroserie::pret_vopsea + Solutii_Caroserie::pret_lac;
-    return k*(pas + 0.8 * Solutii_Caroserie::manopera);
+    return k*pas + 0.3 * Solutii_Caroserie::manopera;
 }
 
 double Solutii_Caroserie::schimb_capota()
 {
     float pas = Solutii_Caroserie::pret_vopsea + Solutii_Caroserie::pret_lac;
-    return Solutii_Caroserie::capota_noua + pas + 0.8 * Solutii_Caroserie::manopera;
+    return Solutii_Caroserie::capota_noua + pas + 0.5 * Solutii_Caroserie::manopera;
 }
 
 double Solutii_Caroserie::schimb_aripa()
 {
     float pas = Solutii_Caroserie::pret_vopsea + Solutii_Caroserie::pret_lac;
-    return Solutii_Caroserie::aripa_noua + pas + 0.8 * manopera;
+    return Solutii_Caroserie::aripa_noua + pas + 0.5 * manopera;
 }
 
 double Solutii_Caroserie::schimb_fataspate()
 {
     float pas = Solutii_Caroserie::pret_vopsea + Solutii_Caroserie::pret_lac;
-    return Solutii_Caroserie::fata_spate + pas + 0.8 * Solutii_Caroserie::manopera;
+    return Solutii_Caroserie::fata_spate + pas + 0.5 * Solutii_Caroserie::manopera;
 }
